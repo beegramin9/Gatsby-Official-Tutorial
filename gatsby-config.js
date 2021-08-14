ff10c62ed8322416d09e41806f37bc2c0d2a7e1b
wontae <beegramin9@gmail.com>
2021-08-14 10:42:38 +0900

    adding plugins for StaticImage

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,7 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby-OfficialTutorial",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud"
+  ],
 };
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from '../components/componentsIndex';
+
+const AboutPage = () => {
+    return (
+        <main>
+            <Layout pageTitle="About Me">
+                <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            </Layout>
+        </main>
+    )
+}
+
+export default AboutPage;
