f59c54a08675bac3d3019a0ca052ee8514b76b3d
wontae <beegramin9@gmail.com>
2021-08-17 22:26:42 +0900

    fuck prismjs

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-//* https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
-//  gatsby-browjer.js: provides options for interacting with the client-side of Gatsby.
-// import "prismjs/themes/prism-tomorrow.css";
